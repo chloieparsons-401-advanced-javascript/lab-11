67b407a850864251c1b71f654d284a002590ce2b
Chloie Parsons <chloieparsons@gmail.com>
2019-06-10 17:03:48 -0700

    Middleware JSDOC done

--- src/auth/middleware.js
+++ src/auth/middleware.js
@@ -2,17 +2,26 @@
 
 const User = require('./users-model.js');
 
+/**
+ * Exports middleware auth check
+ * @param {} req
+ * @param {} res
+ * @param {} next
+ * @param {} authString
+ * @param {} case
+ */
+
 module.exports = (req, res, next) => {
 
   try {
 
-    let [authType, encodedString] = req.headers.authorization.split(/\s+/);
+    let [authType, authString] = req.headers.authorization.split(/\s+/);
 
     // BASIC Auth  ... Authorization:Basic ZnJlZDpzYW1wbGU=
 
     switch(authType.toLowerCase()) {
       case 'basic':
-        return _authBasic(encodedString);
+        return _authBasic(authString);
       default:
         return _authError();
     }
@@ -21,25 +30,44 @@ module.exports = (req, res, next) => {
     return _authError();
   }
 
-  function _authBasic() {
+  /**
+   * Check basic authentication
+   * @param {} authString 
+   * @param {} base64Buffer
+   * @param {} password
+   */
+  function _authBasic(authString) {
     let base64Buffer = Buffer.from(authString,'base64'); // <Buffer 01 02...>
     let bufferString = base64Buffer.toString(); // john:mysecret
     let [username,password] = bufferString.split(':');  // variables username="john" and password="mysecret"
-    let auth = [username,password];  // {username:"john", password:"mysecret"}
+    let auth = {username, password};  // {username:"john", password:"mysecret"}
 
     return User.authenticateBasic(auth)
       .then( user => _authenticate(user) );
   }
 
+  /**
+   * Check if the current user is authenticated
+   * @param {} user 
+   * @param {} req.user
+   */
   function _authenticate(user) {
     if ( user ) {
+      req.user = user;
+      req.token = user.generateToken()
       next();
-    }
-    else {
+    
+    } else {
       _authError();
     }
   }
 
+  /**
+   * Throw error, return 401
+   * @param {401}
+   * @param {'Unauthorized'}
+   * @param {'Invalid User ID/Password'}
+   */
   function _authError() {
     next({status: 401, statusMessage: 'Unauthorized', message: 'Invalid User ID/Password'});
   }
